e008f1d61728f66e1bdac13204c710247a3e2b0f
Caryensong <caryensong@googlemail.com>
2025-02-16 22:58:57 +0100

    update contactfrom

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { ContactformComponent } from './contactform/contactform.component';
 
 export const routes: Routes = [
-    {path:"", component: ContactformComponent}
+     {path:"", component: ContactformComponent}
 ];
