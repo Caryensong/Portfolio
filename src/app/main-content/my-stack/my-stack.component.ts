764bae24ded0132b1fc32156d776fd2a09468f4b
Caryensong <caryensong@googlemail.com>
2025-02-17 12:36:52 +0100

    add main component/ project/about-me/Colleagues/contact

---
+++ src/app/main-content/my-stack/my-stack.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-stack',
+  imports: [],
+  templateUrl: './my-stack.component.html',
+  styleUrl: './my-stack.component.scss'
+})
+export class MyStackComponent {
+
+}
