efc380860495d05c40358422cb373faa1e1c123c
Caryensong <caryensong@googlemail.com>
2025-02-18 13:36:08 +0100

    add myPic mouseover & out effects

--- src/app/main-content/hero/hero.component.ts
+++ src/app/main-content/hero/hero.component.ts
@@ -3,10 +3,16 @@ import { HeaderComponent } from '../../shared/components/header/header.component
 
 @Component({
   selector: 'app-hero',
+  standalone:true,
   imports: [HeaderComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
+  imageSrc: string = './assets/img/me_BW.png'; 
 
+  changeImage(hover: boolean) {
+    this.imageSrc = hover ? './assets/img/me.png' : './assets/img/me_BW.png';
+  }
+  
 }
