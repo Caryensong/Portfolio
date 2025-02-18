5798209286205694d7e5814c18480bb506b3ced6
Caryensong <caryensong@googlemail.com>
2025-02-18 14:28:36 +0100

    add Hello Word / I'm Caryen Song btn

--- src/app/main-content/hero/hero.component.ts
+++ src/app/main-content/hero/hero.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
   standalone:true,
-  imports: [HeaderComponent],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
   imageSrc: string = './assets/img/me_BW.png'; 
+  buttonText: string = 'Hello World';
+  isHovered: boolean = false;
 
   changeImage(hover: boolean) {
     this.imageSrc = hover ? './assets/img/me.png' : './assets/img/me_BW.png';
   }
+
+  changeButtonState(hover: boolean) {
+    this.isHovered = hover;
+  }
+  
   
 }
