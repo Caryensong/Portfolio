0a40eef3ab94c1290c3961a5f983aaeb1804363d
Caryensong <caryensong@googlemail.com>
2025-02-19 22:53:03 +0100

    add Colleagues' Thoughts style

--- src/app/main-content/colleagues/colleagues.component.ts
+++ src/app/main-content/colleagues/colleagues.component.ts
@@ -1,11 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-colleagues',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './colleagues.component.html',
   styleUrl: './colleagues.component.scss'
 })
 export class ColleaguesComponent {
+  cards:{
+    name:string;
+    image:string;
+    linkedIn:string;
+    job:string;
+    text:string;
+  }[]=[
+ {   
+  name:'Thomas',
+  image:'./assets/img/cardB.png',
+  linkedIn:'http/w',
+  job: 'Frontend Developer',
+  text:'supi supi',
+ },
+ {   
+  name:'Karl',
+  image:'./assets/img/cardA.png',
+  linkedIn:'http/w',
+  job: 'Designer',
+  text:'It was a pleasure to work with Caryen...',
+ },
+ {   
+  name:'Sarah',
+  image:'./assets/img/cardB.png',
+  linkedIn:'http/w',
+  job: 'Project Manager',
+  text:'Caryen was a great team colleague...',
+ },
+
+  ];
 
 }
