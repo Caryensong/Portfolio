aacf89e5dfb378aa8c7d09529e9dbcb34ba1b410
Caryensong <caryensong@googlemail.com>
2025-02-19 09:55:38 +0100

    add smooth hover effect with 4s visibility -about me component

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
+  isHovered:boolean = false;
+  showAnimation: boolean = false;
 
+  showImage() {
+    if (!this.showAnimation) { 
+      this.isHovered = true;
+      this.showAnimation = true;
+      
+      setTimeout(() => {
+        this.isHovered = false;
+        this.showAnimation = false;
+      }, 4000);
+    }
+  }
 }
