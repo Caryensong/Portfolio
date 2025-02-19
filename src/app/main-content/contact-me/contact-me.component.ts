f2738b51d621024a847b69a7ce511224bf728ed8
Caryensong <caryensong@googlemail.com>
2025-02-19 13:00:43 +0100

    add contact and colleagues components

--- src/app/main-content/contact-me/contact-me.component.ts
+++ src/app/main-content/contact-me/contact-me.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact-me',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
