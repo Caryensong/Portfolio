6b711c9c860d4306e2bdcfe972c57422fb83120c
Caryensong <caryensong@googlemail.com>
2025-02-20 00:12:55 +0100

    add footer style

--- src/app/main-content/contact-me/contact-me.component.ts
+++ src/app/main-content/contact-me/contact-me.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-contact-me',
   standalone:true,
-  imports: [CommonModule],
+  imports: [CommonModule, FooterComponent],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
