07b1626576b3066d3f7eefa3eb926ee997219f4b
Caryensong <caryensong@googlemail.com>
2025-02-24 11:14:43 +0100

    add project_join Contents

--- src/app/main-content/projects/join/join.component.ts
+++ src/app/main-content/projects/join/join.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-join',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent ],
   templateUrl: './join.component.html',
   styleUrl: './join.component.scss'
 })
 export class JoinComponent {
+  isHovered:boolean = false;
+  showAnimation: boolean = false;
 
+  showImage() {
+    if (!this.showAnimation) { 
+      this.isHovered = true;
+      this.showAnimation = true;
+      
+      setTimeout(() => {
+        this.isHovered = false;
+        this.showAnimation = false;
+      }, 4000);
+    }
+  }
 }
