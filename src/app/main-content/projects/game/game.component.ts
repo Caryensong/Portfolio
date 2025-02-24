847a44e6eac2575f92a79a4fa427037c003d34fd
Caryensong <caryensong@googlemail.com>
2025-02-24 13:17:49 +0100

    add Game project component

--- src/app/main-content/projects/game/game.component.ts
+++ src/app/main-content/projects/game/game.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-game',
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
+  isHovered:boolean = false;
+  showAnimation: boolean = false;
 
+  showImage() {
+    if (!this.showAnimation) { 
+      this.isHovered = true;
+      this.showAnimation = true;
+      
+      setTimeout(() => {
+        this.isHovered = false;
+        this.showAnimation = false;
+      }, 4000);
+    }
+  }
 }
