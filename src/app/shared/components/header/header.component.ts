950ba00a7a0a5e2d1ffe0e3ac795765fc8f6af07
Caryensong <caryensong@googlemail.com>
2025-02-24 23:24:56 +0100

    Add active nav highlighting on scroll & language toggle

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
-import { TranslateDirective, TranslatePipe } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { TranslateDirective, TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone:true,
-  imports: [TranslatePipe, TranslateDirective],
+  imports: [CommonModule, TranslatePipe, TranslateDirective],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  currentLanguage: string = 'en';
+  activeSection = '';
 
+constructor(private translate: TranslateService){
+  this.currentLanguage = this.translate.currentLang || 'en';
+}
+
+changeLanguage(language: string){
+  this.translate.use(language);
+  this.currentLanguage = language;
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    const sections = document.querySelectorAll('section'); 
+    let scrollPosition = window.scrollY + 200;
+
+    sections.forEach((section) => {
+      const sectionId = section.getAttribute('id');
+      if (!sectionId) return;
+
+      const offsetTop = section.offsetTop;
+      const offsetHeight = section.offsetHeight;
+
+      if (scrollPosition >= offsetTop && scrollPosition < offsetTop + offsetHeight) {
+        this.activeSection = sectionId;
+      }
+    });
+  }
 }
