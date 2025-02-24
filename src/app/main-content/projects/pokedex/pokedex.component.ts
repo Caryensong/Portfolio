bf2c2a7dd8c7945d7f77c94caa38523d0f26bde8
Caryensong <caryensong@googlemail.com>
2025-02-24 13:46:56 +0100

    add Pokedex project component

--- src/app/main-content/projects/pokedex/pokedex.component.ts
+++ src/app/main-content/projects/pokedex/pokedex.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-pokedex',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent],
   templateUrl: './pokedex.component.html',
   styleUrl: './pokedex.component.scss'
 })
 export class PokedexComponent {
+  isHovered:boolean = false;
+  showAnimation: boolean = false;
 
+  showImage() {
+    if (!this.showAnimation) { 
+      this.isHovered = true;
+      this.showAnimation = true;
+      
+      setTimeout(() => {
+        this.isHovered = false;
+        this.showAnimation = false;
+      }, 4000);
+    }
+  }
 }
