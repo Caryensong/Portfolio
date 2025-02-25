1ba11e7ff430f02c7ba173abc7b0e3595ab51042
Caryensong <caryensong@googlemail.com>
2025-02-25 22:08:36 +0100

    update footer links hover

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  hoverStates: { [key: string]: boolean } = {};
 
+  changeButtonState(link: string, hover: boolean) {
+    this.hoverStates[link] = hover;
+  }
+
+  activeSection: string = 'contact'; // Setze den aktiven Bereich hier
+
+  setActiveSection(section: string) {
+    this.activeSection = section;
+  }
 }
