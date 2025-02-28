f4897f99169f40a34361681d9aaaf53ebdf0eb8d
Caryensong <caryensong@googlemail.com>
2025-02-28 19:37:56 +0100

    update responsive project

--- src/app/main-content/projects/join/join.component.ts
+++ src/app/main-content/projects/join/join.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { TranslateDirective, TranslatePipe } from '@ngx-translate/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-join',
   standalone: true,
-  imports: [RouterModule, HeaderComponent, TranslateDirective, TranslatePipe],
+  imports: [RouterModule, HeaderComponent, TranslateDirective,NgClass, TranslatePipe],
   templateUrl: './join.component.html',
   styleUrl: './join.component.scss'
 })
 export class JoinComponent {
   isHovered:boolean = false;
   showAnimation: boolean = false;
+  isScrolledDown = false;
+  lastScrollTop = 0;
+
+    @HostListener('window:scroll', [])
+      onScroll() {
+        const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    
+        if (scrollTop > this.lastScrollTop) {
+          this.isScrolledDown = true;
+        } else {
+          this.isScrolledDown = false;
+        }
+    
+        this.lastScrollTop = scrollTop;
+      }
 
   showImage() {
     if (!this.showAnimation) { 
--- src/app/main-content/projects/pokedex/pokedex.component.ts
+++ src/app/main-content/projects/pokedex/pokedex.component.ts
@@ -1,24 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { TranslateDirective, TranslatePipe } from '@ngx-translate/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-pokedex',
   standalone: true,
-  imports: [RouterModule, HeaderComponent, TranslatePipe, TranslateDirective],
+  imports: [RouterModule, HeaderComponent, TranslatePipe, NgClass, TranslateDirective],
   templateUrl: './pokedex.component.html',
   styleUrl: './pokedex.component.scss'
 })
 export class PokedexComponent {
-  isHovered:boolean = false;
+  isScrolledDown = false;
+  lastScrollTop = 0;
+  isHovered: boolean = false;
   showAnimation: boolean = false;
 
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollTop > this.lastScrollTop) {
+      this.isScrolledDown = true;
+    } else {
+      this.isScrolledDown = false;
+    }
+
+    this.lastScrollTop = scrollTop;
+  }
+
   showImage() {
-    if (!this.showAnimation) { 
+    if (!this.showAnimation) {
       this.isHovered = true;
       this.showAnimation = true;
-      
+
       setTimeout(() => {
         this.isHovered = false;
         this.showAnimation = false;
